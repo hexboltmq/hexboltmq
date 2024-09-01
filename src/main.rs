9c7f2b3e1f646fba941c4c1e447e8e7055c81a0f
Adib Grouz <contact@adib-grouz.com>
2024-09-01 13:50:37 +0200

    update structure and add modules

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-mod core;
-
-use core::{Queue, Message};
+use log::info;
 
 fn main() {
-    let mut queue = Queue::new("example_queue");
-    println!("Queue name: {}", queue.name);
-
-    let message = Message {
-        id: "1".to_string(),
-        body: b"Hello, HexboltMQ!".to_vec(),
-    };
-    queue.enqueue(message);
-    println!("Message count: {}", queue.get_message_count());
-    println!("Dequeued message: {:?}", queue.dequeue());
-    println!("Message count: {}", queue.get_message_count());
+    env_logger::init();
+    info!("HexboltMQ is starting...");
 }
